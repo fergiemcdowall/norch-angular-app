a63a609bdaa4d394ae7098791238cf8a10521cf2
Fergie McDowall <fergusmcdowall@gmail.com>
2015-02-08 23:40:25 +0100

    l8st

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,11 +6,14 @@ var searchcatControllers = angular.module('searchcatControllers', []);
 
 searchcatControllers.controller('SearchListCtrl', ['$scope', '$location', 'Search',
   function($scope, $location, Search) {
-    console.log($location.absUrl());
-    $scope.baseurl = $location.absUrl();
+    var queryObject = $location.search();
+    $scope.searchTerm = queryObject['q'] || '';
     $scope.$watch("searchTerm", function(){ 
       if ($scope.searchTerm) if ($scope.searchTerm.length > 2) {
-        $scope.searchresult = Search.srch.query({q: $scope.searchTerm});
+        queryObject['q'] = $scope.searchTerm;
+        console.log(queryObject);
+        $scope.baseurl = $location.absUrl() + '?q=' + queryObject['q'];
+        $scope.searchresult = Search.srch.query(queryObject);
         $scope.matches = Search.mtch.query({beginsWith: $scope.searchTerm});
       }
     })
