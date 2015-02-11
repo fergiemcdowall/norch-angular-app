560b795b8debd8eb6592dfb15da36c8817930711
Fergie McDowall <fergusmcdowall@gmail.com>
2015-02-11 12:10:11 +0100

    facets and filters now working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,15 +8,38 @@ searchcatControllers.controller('SearchListCtrl', ['$scope', '$location', 'Searc
   function($scope, $location, Search) {
     var queryObject = $location.search();
     $scope.searchTerm = queryObject['q'] || '';
+
+
+    //watch the search box
     $scope.$watch("searchTerm", function(){ 
       if ($scope.searchTerm) if ($scope.searchTerm.length > 2) {
         queryObject['q'] = $scope.searchTerm;
-        console.log(queryObject);
-        $scope.baseurl = $location.absUrl() + '?q=' + queryObject['q'];
         $scope.searchresult = Search.srch.query(queryObject);
         $scope.matches = Search.mtch.query({beginsWith: $scope.searchTerm});
       }
     })
+
+
+    //for the facet/filter links
+    $scope.getFilterUrl = function(facetGroupTitle, facetEntry, filters) {
+      var url = '/app/#/search?q=' 
+        + $scope.searchTerm
+        + '&filter[' + facetGroupTitle + '][]=' + facetEntry.key;
+      for (var filterCat in filters) {
+        for (var i in filters[filterCat]) {
+          url += '&filter[' + filterCat + '][]=' + filters[filterCat][i];
+        }
+      }
+      return url;
+    };
+
+
+    //for the [remove] links
+    $scope.getUnFilterUrl = function(facetGroupTitle, facetEntry) {
+      return unescape($location.absUrl()).replace
+      ('&filter[' + facetGroupTitle + '][]=' + facetEntry.key, '');
+    };
+
+
     $scope.orderProp = 'age';
   }]);
-
